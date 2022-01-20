917a46a3bac17e6c9df69a178ebce105956de38c
Vitaly <97674144+Slonevich@users.noreply.github.com>
2022-01-20 18:43:01 +0300

    Version 1.4
    + test mode in 1C processing func, added and change flag "yes/no"
    + order_processing func now append row to existing file instead of rewrite it and table now sorted by ID value
    + few settings to manual input widjet: size of columns based on qtablewidjet wigth, id column now "read only"

--- MainCode.py
+++ MainCode.py
@@ -1,15 +1,19 @@
 # -*- coding: utf-8 -*-
 import email
 import imaplib
-import numpy as np
 import pandas as pd
 import os
 import os.path
 import xlsxwriter
 from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtWidgets import QTableWidgetItem, QMessageBox
+from PyQt5.QtWidgets import QTableWidgetItem, QMessageBox, QStyledItemDelegate
 from tkinter import Tk
 from tkinter.filedialog import askopenfilename
+
+
+class ReadOnlyDelegate(QStyledItemDelegate):
+    def createEditor(self, parent, option, index):
+        return
 
 
 class Ui_MainWindow(object):
@@ -52,7 +56,15 @@ class Ui_MainWindow(object):
         self.Order_to_processing = QtWidgets.QTableWidget(self.centralwidget)
         self.Order_to_processing.setGeometry(QtCore.QRect(10, 330, 300, 150))
         self.Order_to_processing.setStyleSheet("background-color: rgb(158, 251, 255);")
-        self.Order_to_processing.setObjectName("Orders_list_to_processing")
+        self.Order_to_processing.setObjectName("Order_to_processing")
+        self.Order_to_processing.setRowCount(1)
+        self.Order_to_processing.setColumnCount(2)
+        self.Order_to_processing.setHorizontalHeaderLabels(['ID', 'Order number'])
+        self.Order_to_processing.setColumnWidth(0, 140)
+        self.Order_to_processing.setColumnWidth(1, 140)
+        delegate = ReadOnlyDelegate()
+        self.Order_to_processing.setItemDelegateForColumn(0, delegate)
+        # self.Order_to_processing.setEditTriggers(QtWidgets.QTableWidget.NoEditTriggers)
         self.pushButton_3 = QtWidgets.QPushButton(self.centralwidget)
         self.pushButton_3.setGeometry(QtCore.QRect(10, 490, 300, 150))
         font = QtGui.QFont()
@@ -84,14 +96,12 @@ class Ui_MainWindow(object):
 
     def processing_in_1с(self, order_list_f_name):  # обработка в 1С + отметка
         df = pd.read_excel(order_list_f_name)
-        df.at[1, 'Processed'] = 'YES'
-        # for i in range(1, df.shape[0]+1):
-        #     df.iloc[i, "Processed"] = 'YES'
-            # df.loc[i, 'Processed'] = 'YES'
-        df.to_excel(scrip_dir+'\OrderNumbers.xlsx')  # экспорт в Excel
+        for i in range(len(df)):
+            df.at[i, 'Processed'] = 'YES'
+        df.to_excel(script_dir+'\OrderNumbers.xlsx', index=False)  # экспорт в Excel без столбца индексов
         self.display_table(order_list_f_name)
 
-    def file_to_open(self):
+    def file_to_open(self):  # функция открытия Excel файла с проверкой расширения
         Tk().withdraw()  # we don't want a full GUI, so keep the root window from appearing
         fname = askopenfilename()
         filename, file_extension = os.path.splitext(fname)
@@ -106,7 +116,7 @@ class Ui_MainWindow(object):
             error.buttonClicked.connect(self.popup_action)
             error.exec_()
 
-    def display_table(self, order_list_f_name):  # добавляет данные в QTableWidget из выбранной таблицы
+    def display_table(self, order_list_f_name):  # добавляет данные в виджет программы из выбранной таблицы
         df = pd.read_excel(order_list_f_name)
         if df.size == 0:
             return
@@ -135,28 +145,31 @@ def order_processing():  # основная функция - обработка 
         result, data = mail.search(None, "ALL")  # Получаем массив со списком найденных почтовых сообщений
         ids = data[0]  # Сохраняем в переменную ids строку с номерами писем
         id_list = ids.split()  # Получаем массив номеров писем
-        df = pd.DataFrame(columns=['Order number', 'Processed'])  # создаём датафрейм с названным столбцом
+        df = pd.DataFrame(columns=['ID', 'Order number', 'Processed'])  # создаём датафрейм с названным столбцом
 
-        for num in range(len(id_list)):
-            latest_email_id = id_list[
-                -1 - num]  # Задаем переменную latest_email_id, значением будет номер последнего письма
+        for num in range(1, len(id_list)+1):
+            latest_email_id = id_list[-num]  # Задаем переменную, значением которой будет номер последнего письма
 
             result, data = mail.fetch(latest_email_id,
                                       "(RFC822)")  # Получаем письмо с идентификатором latest_email_id (последнее письмо).
             raw_email = data[0][1]  # В переменную raw_email заносим необработанное письмо
             raw_email_string = raw_email.decode(
                 'utf-8')  # Переводим текст письма в кодировку UTF-8 и сохраняем в переменную raw_email_string
-            df.loc[len(df)] = [order_number(raw_email_string), 'NO']
+            df.loc[len(df)] = [num, order_number(raw_email_string), 'NO']
         # copy_and_delete_mail(latest_email_id)  # переносим в папку и удаляем обработанное письмо
-
-        # df.index = np.arange(1, len(df) + 1)  # начинаем номерацию индексов с единицы
-        df.index.name = 'ID'  # задаём название столбца индексов
-        check_file = os.path.exists(scrip_dir+'\OrderNumbers.xlsx')
-        if not check_file:
+        check_file = os.path.exists(script_dir+'\OrderNumbers.xlsx')
+        if not check_file:  # проверка существует ли уже файл выгрузки
             workbook = xlsxwriter.Workbook('OrderNumbers.xlsx')
             worksheet = workbook.add_worksheet()
             workbook.close()
-        df.to_excel(scrip_dir+'\OrderNumbers.xlsx')  # экспорт в Excel
+            df.to_excel(script_dir+'\OrderNumbers.xlsx', index=False)  # экспорт в Excel
+        else:
+            df2 = pd.read_excel(order_list_f_name)
+            df.loc[df['ID'] <= len(df2)+1, 'ID'] = df['ID']+len(df2)
+            frames = [df, df2]
+            result = pd.concat(frames)  # объединение данных файла и обработки
+            result = result.sort_values(by=['ID'])  # сортируем по колонке ID
+            result.to_excel(script_dir+'\OrderNumbers.xlsx', index=False)  # экспорт в Excel
         mail.close()  # закрываем соединение
 
 
@@ -191,6 +204,6 @@ if __name__ == '__main__':
     MainWindow.show()
     order_list_f_name = 'OrderNumbers.xlsx'  # называем файл для выгрузки и обработки заказов
     worksheet_name = 'Sheet1'
-    scrip_dir = os.path.abspath(os.curdir)  # задаём переменной значение директории файла скрипта
+    script_dir = os.path.abspath(os.curdir)  # задаём переменной значение директории файла скрипта
     sys.exit(app.exec_())
 
