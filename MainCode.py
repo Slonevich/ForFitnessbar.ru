20bdb62a84734d680f330aee4390768a18ce465c
Vitaly <97674144+Slonevich@users.noreply.github.com>
2022-01-25 17:17:42 +0300

    Version 1.6
    - added check for the presence of the config.ini file (the script will create the config if not). Mail settings are entered by user when creating a config
    - mail settings, working directory and the name of the order processing file in the code are always imported from the config file

--- MainCode.py
+++ MainCode.py
@@ -8,7 +8,8 @@ import xlsxwriter
 import configparser
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtWidgets import QTableWidgetItem, QMessageBox, QStyledItemDelegate
-from tkinter import Tk, filedialog
+import tkinter as tk
+from tkinter import Tk, filedialog, simpledialog
 from tkinter.filedialog import askopenfilename
 from functools import partial
 
@@ -94,7 +95,7 @@ class Ui_MainWindow(object):
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
-        self.display_table(order_list_f_name)  # отображаем файл со списком заказов, если он есть - либо новый, пустой
+        self.display_table()  # отображаем файл со списком заказов, если он есть - либо новый, пустой
         self.add_function()  # добавляем функционал кнопкам
 
     def retranslateUi(self, MainWindow):
@@ -106,48 +107,50 @@ class Ui_MainWindow(object):
         self.pushButton_3.setText(_translate("MainWindow", "Обработать в 1C"))
         self.pushButton_4.setText(_translate("MainWindow", "добавить заказ"))
         self.checkBox.setText(_translate("MainWindow", "Путь к файлу по умолчанию"))
-        self.label_2.setText(_translate("MainWindow", work_dir))
-
+        self.label_2.setText(_translate("MainWindow", config['DEFAULT']['work_dir']))
 
     def add_function(self):  # добавляем функции кнопкам
-        # self.pushButton.clicked.connect(lambda: self.dir_to_use())
         self.pushButton.clicked.connect(lambda: order_processing())
-        self.pushButton.clicked.connect(lambda: self.display_table(order_list_f_name))
+        self.pushButton.clicked.connect(lambda: self.display_table())
         self.pushButton_2.clicked.connect(lambda: self.file_to_open())
-        self.pushButton_3.clicked.connect(lambda: self.processing_in_1с(order_list_f_name))
-        self.pushButton_4.clicked.connect(lambda: self.add_order(order_list_f_name))
+        self.pushButton_3.clicked.connect(lambda: self.processing_in_1c())
+        self.pushButton_4.clicked.connect(lambda: self.add_order())
         enable_slot = partial(self.enable_mod)
         disable_slot = partial(self.disable_mod)
         self.checkBox.stateChanged.connect(lambda x: enable_slot() if x else disable_slot())
 
     def enable_mod(self):
         self.checkBox.setText("Путь к файлу по умолчанию")
-        self.label_2.setText(work_dir)
+        self.label_2.setText(os.path.abspath(os.curdir))
+        config['DEFAULT']['work_dir'] = os.path.abspath(os.curdir)
+        with open('config.ini', 'w') as configfile:
+            config.write(configfile)
 
     def disable_mod(self):
         Tk().withdraw()
         new_dir_name = filedialog.askdirectory()
         self.label_2.setText("Новый путь к файлу: " + new_dir_name)
-        global work_dir
-        work_dir = new_dir_name
+        config['DEFAULT']['work_dir'] = new_dir_name
+        with open('config.ini', 'w') as configfile:
+            config.write(configfile)
 
-    def add_order(self, order_list_f_name):  # добавление файла к обработке ручным вводом
+    def add_order(self):  # добавление файла к обработке ручным вводом
         df2 = pd.DataFrame(columns=['ID', 'Order number', 'Processed'])  # создаём датафрейм с названным столбцом
-        df = pd.read_excel(work_dir + '\\' + order_list_f_name)
+        df = pd.read_excel(config['DEFAULT']['work_dir'] + '\\' + config['DEFAULT']['order_list_f_name'])
         self.Order_to_processing.setItem(0, 0, QtWidgets.QTableWidgetItem(str(len(df)+1)))  # ID по таблице
         df2.loc[0] = [int(self.Order_to_processing.item(0, 0).text()), int(self.Order_to_processing.item(0, 1).text()), 'NO']
         frames = [df, df2]
         result = pd.concat(frames)  # объединение данных файла и обработки
         result = result.sort_values(by=['ID'])  # сортируем по колонке ID
-        result.to_excel(work_dir + '\\' + order_list_f_name, index=False)  # экспорт в Excel
-        self.display_table(order_list_f_name)
+        result.to_excel(config['DEFAULT']['work_dir'] + '\\' + config['DEFAULT']['order_list_f_name'], index=False)  # экспорт в Excel
+        self.display_table()
 
-    def processing_in_1с(self, order_list_f_name):  # обработка в 1С + отметка
-        df = pd.read_excel(work_dir + '\\' + order_list_f_name)
+    def processing_in_1c(self):  # обработка в 1С + отметка
+        df = pd.read_excel(config['DEFAULT']['work_dir'] + '\\' + config['DEFAULT']['order_list_f_name'])
         for i in range(len(df)):
             df.at[i, 'Processed'] = 'YES'
-        df.to_excel(work_dir + '\\' + order_list_f_name, index=False)  # экспорт в Excel без столбца индексов
-        self.display_table(order_list_f_name)
+        df.to_excel(config['DEFAULT']['work_dir'] + '\\' + config['DEFAULT']['order_list_f_name'], index=False)  # экспорт в Excel без столбца индексов
+        self.display_table()
 
     def file_to_open(self):  # функция открытия Excel файла с проверкой расширения
         Tk().withdraw()
@@ -156,20 +159,23 @@ class Ui_MainWindow(object):
         fname = os.path.split(file_path)[1]
         filename, file_extension = os.path.splitext(file_path)
         if file_extension == '.xlsx':
-            global work_dir
-            work_dir = directory
-            self.display_table(fname)
+            # global work_dir
+            config['DEFAULT']['work_dir'] = directory
+            config['DEFAULT']['order_list_f_name'] = fname
+            with open('config.ini', 'w') as configfile:
+                config.write(configfile)
+            self.display_table()
         else:
             error = QMessageBox()
             error.setWindowTitle('Ошибка')
             error.setText('Выбран файл неверного формата. Выберите файл .xlsx')
             error.setIcon(QMessageBox.Warning)
             error.setStandardButtons(QMessageBox.Ok)
-            error.buttonClicked.connect(self.popup_action)
+            error.buttonClicked.connect(self.error_popup_action)
             error.exec_()
 
-    def display_table(self, order_list_f_name):  # добавляет данные в виджет программы из выбранной таблицы
-        df = pd.read_excel(work_dir + '\\' + order_list_f_name)
+    def display_table(self):  # добавляет данные в виджет программы из выбранной таблицы
+        df = pd.read_excel(config['DEFAULT']['work_dir'] + '\\' + config['DEFAULT']['order_list_f_name'])
         df.fillna('', inplace=True)  # заменяем\смещаем пустые ячейки
         self.Orders_list_to_processing.setRowCount(df.shape[0])  # задаём количество строк из файла
         self.Orders_list_to_processing.setColumnCount(df.shape[1])  # задаём количество столбцов из файла
@@ -181,14 +187,14 @@ class Ui_MainWindow(object):
                 tableitem = QTableWidgetItem(str(value))
                 self.Orders_list_to_processing.setItem(row[0], col_index, tableitem)
 
-    def popup_action(self, btn):  # кнопка всплывающего окна ошибки
+    def error_popup_action(self, btn):  # кнопка всплывающего окна ошибки
         if btn.text() == "OK":
             self.file_to_open()
 
 
 def order_processing():  # основная функция - обработка заказов с почты
-        mail = imaplib.IMAP4_SSL('mail.fitnessbar.ru')
-        mail.login('region-zakaz@fitnessbar.ru', '222QQq222')  # заходим на сервер
+        mail = imaplib.IMAP4_SSL(config['MAIL']['mail_server_name'])
+        mail.login(config['MAIL']['login'], config['MAIL']['password'])  # заходим на сервер
         mail.list()  # получаем список папок
         mail.select('inbox', readonly=False)  # выбираем папку
         result, data = mail.search(None, "ALL")  # Получаем массив со списком найденных почтовых сообщений
@@ -200,30 +206,49 @@ def order_processing():  # основная функция - обработка 
             latest_email_id = id_list[-num]  # Задаем переменную, значением которой будет номер последнего письма
 
             result, data = mail.fetch(latest_email_id,
-                                      "(RFC822)")  # Получаем письмо с идентификатором latest_email_id (последнее письмо).
+                                      "(RFC822)")  # Получаем последнее письмо).
             raw_email = data[0][1]  # В переменную raw_email заносим необработанное письмо
             raw_email_string = raw_email.decode(
                 'utf-8')  # Переводим текст письма в кодировку UTF-8 и сохраняем в переменную raw_email_string
             df.loc[len(df)] = [num, order_number(raw_email_string), 'NO']
         # copy_and_delete_mail(latest_email_id)  # переносим в папку и удаляем обработанное письмо
-        df2 = pd.read_excel(work_dir + '\\' + order_list_f_name)
+        df2 = pd.read_excel(config['DEFAULT']['work_dir'] + '\\' + config['DEFAULT']['order_list_f_name'])
         df.loc[df['ID'] <= len(df2)+1, 'ID'] = df['ID']+len(df2)
         frames = [df, df2]
         result = pd.concat(frames)  # объединение данных файла и обработки
         result = result.sort_values(by=['ID'])  # сортируем по колонке ID
-        result.to_excel(work_dir + '\\' + order_list_f_name, index=False)  # экспорт в Excel
+        result.to_excel(config['DEFAULT']['work_dir'] + '\\' + config['DEFAULT']['order_list_f_name'],
+                        index=False)  # экспорт в Excel
         mail.close()  # закрываем соединение
 
 
+def input_mail_settings():  # функция предлагает пользователю ввести настройки почты для обработки
+    application_window = tk.Tk()
+    application_window.withdraw()
+    answer = simpledialog.askstring("Input", "Введите название почтового сервера: ",
+                                    parent=application_window)
+    if answer:
+        config['MAIL']['mail_server_name'] = answer
+    answer = simpledialog.askstring("Input", "Введите логин почты: ",
+                                    parent=application_window)
+    if answer:
+        config['MAIL']['login'] = answer
+    answer = simpledialog.askstring("Input", "Введите пароль: ",
+                                    parent=application_window)
+    if answer:
+        config['MAIL']['password'] = answer
+
+
 def create_default_config():  # создаём файл со стандартными настройками
-    config = configparser.ConfigParser()
     config['DEFAULT'] = {'order_list_f_name': 'OrderNumbers.xlsx', 'work_dir': os.path.abspath(os.curdir)}
+    config['MAIL'] = {}
+    input_mail_settings()
     with open('config.ini', 'w') as configfile:
         config.write(configfile)
 
 
 def order_number(raw_email_string):  # функция возвращает номер заказа из текста письма
-    email_message = email.message_from_string(raw_email_string)  # Получаем заголовки и тело письма и в перем email
+    email_message = email.message_from_string(raw_email_string)  # Получаем заголовки и тело письма в перем email
     # _message. Записываем в ту же переменную, где было необработанное письмо
 
     from email.header import decode_header
@@ -244,19 +269,26 @@ def copy_and_delete_mail(mail_id):  # Функция копирует письм
         mail.expunge()  # удаляет отмеченные письма
 
 
-if __name__ == '__main__':
-    import sys
-    create_default_config()
-    order_list_f_name = 'OrderNumbers.xlsx'  # называем файл для выгрузки и обработки заказов
-    worksheet_name = 'Sheet1'
-    work_dir = os.path.abspath(os.curdir)  # задаём директории по умолчанию значение директории файла скрипта
-    check_file = os.path.exists(work_dir + '\\' + order_list_f_name)
-    if not check_file:  # проверка существует ли уже файл выгрузки
-        workbook = xlsxwriter.Workbook(work_dir + '\\' + order_list_f_name)
-        worksheet = workbook.add_worksheet()
+def start_app_files_check():  # проверка наличия файлов конфига и таблицы заказов
+    check_config_file = os.path.exists('config.ini')
+    if not check_config_file:
+        create_default_config()
+    config.read('config.ini')
+    check_order_list_file = os.path.exists(config['DEFAULT']['work_dir'] + '\\'
+                                           + config['DEFAULT']['order_list_f_name'])
+    if not check_order_list_file:  # проверка существует ли уже файл выгрузки
+        workbook = xlsxwriter.Workbook(config['DEFAULT']['work_dir'] + '\\' + config['DEFAULT']['order_list_f_name'])
+        workbook.add_worksheet()
         workbook.close()
         empty_df = pd.DataFrame(columns=['ID', 'Order number', 'Processed'])  # создаём датафрейм с названными столбцами
-        empty_df.to_excel(work_dir + '\\' + order_list_f_name, index=False)  # экспорт в Excel
+        empty_df.to_excel(config['DEFAULT']['work_dir'] + '\\' + config['DEFAULT']['order_list_f_name'],
+                          index=False)  # экспорт в Excel
+
+
+if __name__ == '__main__':
+    import sys
+    config = configparser.ConfigParser()
+    start_app_files_check()
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
     ui = Ui_MainWindow()
